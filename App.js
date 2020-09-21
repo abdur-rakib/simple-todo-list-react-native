3da953f1f46cc9b1c4f2f8b0ff9428d0a0917187
abdur-rakib <abdurrakib961@gmail.com>
2020-09-21 22:00:48 +0600

    initialized project

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Welcome to React Native </Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
