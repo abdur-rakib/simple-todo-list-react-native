e95f014b1ee96e8297f1273db350cfc9e632a250
abdur-rakib <abdurrakib961@gmail.com>
2020-09-24 07:36:43 +0600

    listening auth state

--- Container.js
+++ Container.js
@@ -1,17 +1,52 @@
-import React, {useEffect} from 'react';
-import {View, Text} from 'react-native';
-import {connect} from 'react-redux';
+import React, {useEffect, useState} from 'react';
 import MainStackNavigator from './navigation/MainStackNavigator';
 import Login from './screens/Login';
+import {auth} from './firebase/utils';
+import store from './redux/store';
+import {SET_USER} from './redux/types';
+import {Text} from 'react-native-elements';
+import {View} from 'react-native';
 
-const Container = ({auth}) => {
-  useEffect(() => {}, [auth]);
-  const renderScreen = auth.authenticated ? <MainStackNavigator /> : <Login />;
-  return renderScreen;
-};
-const mapStateToProps = (state) => {
-  return {
-    auth: state.auth,
+const Container = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  const onAuthStateChanged = (user) => {
+    setUser(user);
+    if (user) {
+      store.dispatch({
+        type: SET_USER,
+        payload: {
+          authenticated: true,
+          userName: user.disaplayName,
+          uaerImage: user.photoUrl,
+        },
+      });
+    }
+    if (initializing) setInitializing(false);
   };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+  // const renderScreen = auth.authenticated ? <MainStackNavigator /> : <Login />;
+
+  if (initializing) return null;
+
+  if (!user) {
+    return <Login />;
+  }
+
+  return <MainStackNavigator />;
 };
-export default connect(mapStateToProps)(Container);
+
+// const mapStateToProps = (state) => {
+//   return {
+//     auth: state.auth,
+//   };
+// };
+
+export default Container;
--- redux/reducers/dataReducer.js
+++ redux/reducers/dataReducer.js
@@ -1,23 +1,7 @@
 import {ADD_TASK, DELETE_TASK, UPDATE_TASK} from '../types';
 
-const tasks = [
-  {
-    id: 1,
-    taskDescription:
-      'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
-    completed: false,
-    author: 'Abdur Rakib',
-  },
-  {
-    id: 2,
-    taskDescription:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy',
-    completed: false,
-    author: 'Abdur Rakib',
-  },
-];
 const initialState = {
-  tasks: tasks,
+  tasks: [],
   singleTask: null,
 };
 
@@ -34,9 +18,6 @@ export default function (state = initialState, action) {
         tasks: state.tasks.filter((task) => task.id !== action.payload),
       };
     case UPDATE_TASK:
-      // console.log(state.tasks[0]);
-      // console.log(action.payload.id);
-
       let taskIndex = state.tasks.findIndex(
         (task) => task.id === action.payload.id,
       );
